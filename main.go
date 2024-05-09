8743a125f600b963dd0c5398a6bbdffe1e181624
Shubham Phadte <shubhamphadte37@gmail.com>
2024-05-09 16:45:30 +0530

    main.go formHandler logic

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request){
 	if r.URL.Path !="/hello" {
-		http.Error(w, "404 not found",http.StatusNotFound)
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return 
 	}
 
@@ -21,6 +21,18 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err!=nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n",name)
+	fmt.Fprintf(w, "Address = %s\n",address)
+	
 
 }
 
